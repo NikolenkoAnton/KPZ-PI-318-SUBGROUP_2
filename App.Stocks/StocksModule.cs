8fc509747261eda3f39e12a2ef93ae88ad69db8d
Anton Nikolenko <nikolenko@kai24.com>
2019-09-26 01:29:47 +0300

    add service module

---
+++ App.Stocks/StocksModule.cs
@@ -0,0 +1,15 @@
+﻿using Castle.Windsor;
+using Castle.MicroKernel.Registration;
+using App.Configuration;
+
+namespace App.Stocks
+{
+    public class StocksModule : IModule
+    {
+        public void Initialize(IWindsorContainer container)
+        {
+            // example of manually registered components
+            //container.Register(Component.For<IAnotherService>().ImplementedBy<AnotherService>().LifestyleTransient());
+        }
+    }
+}
