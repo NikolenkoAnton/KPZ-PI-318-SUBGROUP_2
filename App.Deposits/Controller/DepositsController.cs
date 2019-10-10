547bd6de4af0ae047ba136c86cc00638cea98e5c
Dmitro Ozzmos <dmitry.ozzmos@gmail.com>
2019-10-10 23:52:36 +0300

    Create project, add reference, create base structure, createte model deposits

---
+++ App.Deposits/Controller/DepositsController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Deposits.Controller
+{
+    public class DepositsController
+    {
+    }
+}
---
+++ App.Deposits/DepositsManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Deposits
+{
+    public interface IDepositsManager
+    {
+
+    }
+
+    public class DepositsManager : IDepositsManager
+    {
+    }
+}
