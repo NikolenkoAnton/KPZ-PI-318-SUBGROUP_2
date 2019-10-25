8e86f9b882ef59e2f5d9444eac023e4df06cbebd
Maxim Strikov <maximStrikov@gmail.com>
2019-10-25 10:25:05 +0300

    implemented regular payments module

---
+++ App.RegualrPayments/Models/RegularPayment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace App.RegularPayments.Models
+{
+    public class RegularPayment
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Sum { get; set; }
+        public DateTime Date { get; set; }
+        public bool State { get; set; }
+    }
+}
