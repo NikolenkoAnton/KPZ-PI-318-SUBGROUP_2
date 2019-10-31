17c24b437550e499ebc5e8611034160cbfb9db03
Anton Nikolenko <nikolenko@kai24.com>
2019-10-31 02:41:26 +0200

    Add logger to controller methods

--- App.Stocks/Controllers/StocksController.cs
+++ App.Stocks/Controllers/StocksController.cs
@@ -3,40 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using App.Stocks.Exceptions;
+using App.Stocks.Filters;
 using App.Stocks.Interfaces;
 using App.Stocks.Models;
 using App.Stocks.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace App.Stocks.Controllers
 {
     [Route("api/stocks")]
     [ApiController]
+    [TypeFilter(typeof(StockExceptionFilter), Arguments = new object[] { nameof(StocksController) })]
     public class StocksController : ControllerBase
     {
         readonly ICompaniesManager companyManager;
         readonly IStocksManager stocksManager;
         readonly IValidateServices validateService;
-
+        readonly ILogger<StocksController> logger;
         public StocksController(
             ICompaniesManager companyManager,
             IStocksManager stocksManager,
-            IValidateServices validateService)
+            IValidateServices validateService,
+            ILogger<StocksController> logger)
         {
             this.companyManager = companyManager;
             this.stocksManager = stocksManager;
             this.validateService = validateService;
+            this.logger = logger;
 
         }
 
         [HttpGet("companies/{id}/stocks/all")] 
         public async Task<IEnumerable<StockView>> CompanyStocks(int id)
         {
+            logger.LogInformation($"Call CompanyStocks method with id : {id}");
             return await stocksManager.CompanyStocks(id);
         }
         [HttpGet("companies/{id}/stocks")]
         public async Task<StockView> StockByDate([FromQuery] string Date,int id)
         {
+            logger.LogInformation($"Call StockByDate with id : {id} , date: {Date}");
             validateService.ValidateDate(Date);
             return await stocksManager.CompanyStockByDate(id, DateTime.Parse(Date));
         }
@@ -44,10 +52,11 @@ namespace App.Stocks.Controllers
         [HttpGet("companies/{id}")]
         public async Task<CompanyView> Company(int id)
         {
+            logger.LogInformation($"Call Company(by id) with id : {id}");
             var company = await companyManager.GetCompanyByIdAsync(id);
             if(company == null)
             {
-                throw new HttpListenerException((int)HttpStatusCode.NotFound, "Company doesn't exist");
+                throw new EntityNotExist(typeof(Company), "Company doesn't exist");
             }
             return company;
         }
@@ -55,13 +64,15 @@ namespace App.Stocks.Controllers
         [HttpGet("companies/open")]
         public async Task<IEnumerable<CompanyView>> CompaniesWithOpenStocks()
         {
+            logger.LogInformation($"Call CompaniesWithOpenStocks");
             return await companyManager.GetCompaniesWithOpenStocksAsync();
         }
 
         [HttpGet("companies/all")]
         public async Task<IEnumerable<CompanyView>> AllCompanies()
         {
-           return await companyManager.GetAllCompaniesAsync();
+            logger.LogInformation($"Call AllCompanies");
+            return await companyManager.GetAllCompaniesAsync();
         }
         
         
