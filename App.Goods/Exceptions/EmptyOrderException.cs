c7bb9efdc02590cade06b40747e707734f460200
k1llama <gtamaterasulol@gmail.com>
2019-11-07 16:50:14 +0200

    Add exception classes

---
+++ App.Goods/Exceptions/EmptyOrderException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace App.Goods.Exceptions
+{
+    public class EmptyOrderException : Exception
+    {
+        public int OrderId { get; private set; }
+
+        public EmptyOrderException(int orderId)
+        {
+            OrderId = orderId;
+        }
+    }
+}
---
+++ App.Goods/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace App.Goods.Exceptions
+{
+    class ProductNotFoundException
+    {
+        public IEnumerable<int> ProductIds { get; private set; }
+
+        public ProductNotFoundException(IEnumerable<int> productIds)
+        {
+            ProductIds = productIds;
+        }
+    }
+}
