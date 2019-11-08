72b98856fde0ff53991ff2d8d82870d42609e079
NikitaDocent <docent2584@gmail.com>
2019-11-08 13:25:49 +0200

    Added Filter for Exceptions

--- App.UserSupport/Exceptions/InvalidArgumentException.cs
+++ App.UserSupport/Exceptions/InvalidArgumentException.cs
@@ -6,7 +6,6 @@ namespace App.UserSupport.Exceptions
 {
     public class InvalidArgumentException : ArgumentException
     {
-        
         public InvalidArgumentException(string message, string paramName) : base(message, paramName)
         {
         }
--- App.UserSupport/Filters/UserSupportExceptionFilter.cs
+++ App.UserSupport/Filters/UserSupportExceptionFilter.cs
@@ -1,16 +1,58 @@
 ﻿using System;
+using System.Net;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
 using System.Text;
 using System.Threading.Tasks;
+using App.UserSupport.Exceptions;
+using Microsoft.AspNetCore.Http;
 
 namespace App.UserSupport.Filters
 {
     public class UserSupportExceptionFilter : IAsyncExceptionFilter
     {
-        public Task OnExceptionAsync(ExceptionContext context)
-        {
-            throw new NotImplementedException();
+            readonly string _context;
+            readonly ILogger<UserSupportExceptionFilter> logger;
+            public UserSupportExceptionFilter(ILogger<UserSupportExceptionFilter> logger, string context)
+            {
+                this.logger = logger;
+                _context = context;
+            }
+
+            public async Task OnExceptionAsync(ExceptionContext context)
+            {
+                logger.LogError(context.Exception, $"Error occurred in context of {_context}");
+                switch (context.Exception)
+                {
+                    case ClientAlredyExistException clientAlredyExist:
+                        {
+                            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.NotFound;
+
+                            logger.LogWarning($"Type : {clientAlredyExist.EntityType.AssemblyQualifiedName},EntityId {EntityNotExistException.EntityId}. Method: {EntityNotExistException.TargetSite}.");
+
+                            await context.HttpContext.Response.WriteAsync($"Entity with id : {EntityNotExistException.EntityId} and type {EntityNotExistException.EntityType.AssemblyQualifiedName} not found!");
+                            break;
+                        }
+                    case InvalidArgumentException invalidArgument:
+                        {
+                            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                            logger.LogWarning(invalidArgument, $"Argument name: {invalidArgument.ParamName}. Method: {invalidArgument.TargetSite}");
+                            await context.HttpContext.Response.WriteAsync($@"You send request with incorrect {invalidArgument.ParamName} format");
+                            break;
+                        }
+                    default:
+                        {
+                            logger.LogError($"Method: {context.Exception.TargetSite}.");
+
+                            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                            await context.HttpContext.Response.WriteAsync("Unhandled exception ! Please, contact support for resolve");
+                            break;
+                        }
+                }
+                context.ExceptionHandled = true;
+            }
+
         }
     }
 }
