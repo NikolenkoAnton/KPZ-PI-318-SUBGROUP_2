8cb7b4c89aca93605f0e9713d8e7439d3c7bb824
HudokonNau <55258644+HudokonNau@users.noreply.github.com>
2019-11-12 00:25:32 +0200

    Add Exceptions and Filter

---
+++ App.Loans/Exceptions/LoanAlreadyPaidException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace App.Loans.Exceptions
+{
+    public class LoanAlreadyPaidException : Exception
+    {
+        public int HandlingId { get; private set; }
+        public LoanAlreadyPaidException(int HandlingId)
+        {
+            this.HandlingId = HandlingId;
+        }
+    }
+}
---
+++ App.Loans/Filters/LoansExceptionsFilter.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using App.Loans.Exceptions;
+using Microsoft.AspNetCore.Http;
+
+namespace App.Loans.Filters
+{
+    public class LoansExceptionsFilter : IAsyncExceptionFilter
+    {
+        readonly string _context;
+        readonly ILogger<LoansExceptionsFilter> logger;
+        public LoansExceptionsFilter(ILogger<LoansExceptionsFilter> logger, string context)
+        {
+            this.logger = logger;
+            _context = context;
+        }
+
+        public async Task OnExceptionAsync(ExceptionContext context)
+        {
+            logger.LogError(context.Exception, $"Error occurred in context of {_context}");
+            switch (context.Exception)
+            {
+                case LoanAlreadyPaidException LoanAlreadyPaid:
+                    {
+                        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        await context.HttpContext.Response.WriteAsync($"Loan with id {handlingAlredyComplete.HandlingId} is already paid!");
+                        break;
+                    }
+                case EntityNotFoundException entityNotFound:
+                    {
+                        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                        await context.HttpContext.Response.WriteAsync($"Not Found: {entityNotFound.EntityType.AssemblyQualifiedName}");
+                        break;
+                    }
+                default:
+                    {
+                        logger.LogError($"Method: {context.Exception.TargetSite}.");
+                        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        await context.HttpContext.Response.WriteAsync("Unhandled exception ! Please, contact support for resolve");
+                        break;
+                    }
+            }
+            context.ExceptionHandled = true;
+        }
+
+    }
+}
+
