e1db0ca4568934166cc96b737481a9d37298e404
NikitaDocent <docent2584@gmail.com>
2019-12-05 20:17:38 +0200

    More fixes

--- App.UserSupport/UserSupportModule.cs
+++ App.UserSupport/UserSupportModule.cs
@@ -13,18 +13,26 @@ namespace App.UserSupport
         {
             RegisterDbContext(container);
         }
-        private Handling[] handlings = new Handling[5];
 
         IEnumerable<Handling> InitHandlings()
         {
-            handlings[0] = new Handling(new Client("Nikita"), "Some times i have troubles", 2,1);
+            Handling[] handlings = new Handling[5];
+            Client[] clients = new Client[]{
+                new Client("Nikita"),
+                new Client("Alina"),
+                new Client("Masha"),
+                new Client("Lana"),
+                new Client("Anton")};
+
+            for(int i = 0;i < 5; i++)
+            {
+                handlings[i] = new Handling(clients[i], "Some times i have troubles", i, i);
+            }
+
             handlings[0].status = true;
-            handlings[1] = new Handling(new Client("Alina"), "Some times i make troubles", 3,2);
-            handlings[2] = new Handling(new Client("Masha"), "Every times i have troubles", 4,3);
-            handlings[3] = new Handling(new Client("Lana"), "Some times i have exeptions", 1,4);
-            handlings[4] = new Handling(new Client("Anton"), "Some times i have troubles", 5,5);
-            handlings[1].WriteAnswer("What happend?",6);
-            handlings[2].WriteAnswer("Give me pls more information",7);
+            handlings[1].WriteAnswer("What happend?", 6);
+            handlings[2].WriteAnswer("Give me pls more information", 7);
+
             return handlings;
         }
         private void RegisterDbContext(IWindsorContainer container)
